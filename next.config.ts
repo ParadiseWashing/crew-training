66e806f4aa63dec51ebe8df4443bfd4a4f110466
ParadiseWashing <contact@paradisewashing.com>
2026-05-02 23:29:03 -0700

    fix: remove invalid allowedOrigins and webpack plugin from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "utfs.io" },
-      { protocol: "https", hostname: "uploadthing.com" },
-    ],
-  },
-  experimental: {
-    serverActions: { allowedOrigins: ["localhost:3000"] },
-  },
-  serverExternalPackages: [
-    "@prisma/client",
-    "@prisma/adapter-pg",
-    "pg",
-  ],
-  webpack: (config, { webpack: wp }) => {
-    config.plugins.push(
-      new wp.NormalModuleReplacementPlugin(/^node:(.*)/, (resource: { request: string }) => {
-        resource.request = resource.request.replace(/^node:/, "");
-      })
-    );
-    return config;
-  },
+    images: {
+          remotePatterns: [
+            { protocol: "https", hostname: "utfs.io" },
+            { protocol: "https", hostname: "uploadthing.com" },
+                ],
+    },
+    serverExternalPackages: [
+          "@prisma/client",
+          "@prisma/adapter-pg",
+          "pg",
+          "bcryptjs",
+        ],
 };
 
 export default nextConfig;
